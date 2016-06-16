398b09b9508006d1bbbeae3417d0ae9581c1b6a1
James Elliott <james@deepsymmetry.org>
2016-06-15 22:51:20 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject ola-clojure "0.1.6"
+(defproject ola-clojure "0.1.7-SNAPSHOT"
   :description "A Clojure library for communicating with the Open Lighting Architecture."
   :url "https://github.com/brunchboy/ola-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.cache "0.6.5"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.382"]
                  [org.clojars.brunchboy/protobuf "0.8.3"]
                  [selmer "1.0.4"
                   :exclusions [com.google.protobuf/protobuf-java]]
-                 [com.taoensso/timbre "4.3.1"]]
+                 [com.taoensso/timbre "4.4.0"]]
   :source-paths ["src" "generated"]
   :prep-tasks [["with-profile" "+gen,+dev" "run" "-m" "ola-clojure.src-generator"] "protobuf" "javac" "compile"]
 
