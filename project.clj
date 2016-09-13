b721725612fed576bf4fb2c25dbe3a67f7a4ca94
James Elliott <james@deepsymmetry.org>
2016-09-12 20:13:41 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.cache "0.6.5"]
-                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/core.async "0.2.391"]
                  [org.clojars.brunchboy/protobuf "0.8.3"]
                  [selmer "1.0.7"
                   :exclusions [com.google.protobuf/protobuf-java]]
